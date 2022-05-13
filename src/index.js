1a1cd5e95773a8293d94b6bfed7f0a4c5b0e7df9
walson <walsongodwin@gmail.com>
2022-05-13 16:49:18 +0530

    added stripe backkned

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const app=express()
 const cors=require("cors")
 const mongoose=require("mongoose")
 
+require("dotenv").config()
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST)
 
 const user=require("./Controllers/usercontroller")
 
@@ -12,7 +14,29 @@ app.use(cors())
 app.use("/users",user)
 
 
-
+app.post("/payment", cors(), async (req, res) => {
+	let { amount, id } = req.body
+	try {
+		const payment = await stripe.paymentIntents.create({
+			amount,
+			currency: "INR",
+			description: "SHOE ROOM",
+			payment_method: id,
+			confirm: true
+		})
+		console.log("Payment", payment)
+		res.json({
+			message: "Payment successful",
+			success: true
+		})
+	} catch (error) {
+		console.log("Error", error)
+		res.json({
+			message: "Payment failed",
+			success: false
+		})
+	}
+})
 
 
 const connect=()=>{
